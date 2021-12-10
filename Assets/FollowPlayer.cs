01f902c16bd68dec1cce4a01a0d991b6f302671a
guuzz131 <guuzz@icloud.com>
2021-12-10 13:22:08 +0100

    Robot AI, + Camera focus & improvements

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -8,21 +8,87 @@ public class FollowPlayer : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private float refocusSpeed;
     [SerializeField] private float refocusTime;
+    [SerializeField] private string searchTag;
+    public bool focussingCam;
 
-    bool refocus;
+    public bool focussingHeld;
+
+    public GameObject enemy;
+    Vector3 focusTargetPos;
+    Quaternion slowPlayerRot;
+    void Start()
+    {
+        StartCoroutine(PerSecondUpdate());
+    }
+
+    IEnumerator PerSecondUpdate()
+    {
+        slowPlayerRot = player.rotation;
+        yield return new WaitForSeconds(1f);
+        if(player.rotation == slowPlayerRot && !focussingCam)
+        {
+            slowPlayerRot = player.rotation;
+            transform.DORotateQuaternion(slowPlayerRot, 1);
+        }
+        StartCoroutine(PerSecondUpdate());
+    }
 
     void Update()
     {
-        transform.position = new Vector3(player.position.x, transform.position.y, player.position.z);
+        if (!focussingHeld)
+        {
+            focusTargetPos = player.position;
+        }
+        else
+        {
+            focusTargetPos = (player.position + enemy.transform.position) / 2;
+        }
+
+        transform.position = new Vector3(focusTargetPos.x, focusTargetPos.y, focusTargetPos.z);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             //Vector3 playerRot = new Vector3(0, player.rotation.y, 0);
             //transform.DORotate(playerRot, refocusTime);
+            StartCoroutine(HeldSpace());
+            focussingCam = true;
             Quaternion playerDir = Quaternion.LookRotation(player.GetComponent<PlayerMovement3D>().targetDirection);
             transform.DORotateQuaternion(playerDir, refocusTime);
         }
-
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            focussingCam = false;
+            focussingHeld = false;
+        }
     }
     
+
+    IEnumerator HeldSpace()
+    {
+        yield return new WaitForSeconds(0.1f);
+        if (!focussingCam) yield break;
+        focussingHeld = true;
+        var found = GameObject.FindGameObjectsWithTag(searchTag);
+        if (found.Length == 0)
+        {
+            enemy = null;
+            yield break;
+        }
+
+        GameObject closest = null;
+        var dist = Mathf.Infinity;
+        foreach (var go in found)
+        {
+
+            var newDist = Vector3.Distance(go.transform.position, player.position);
+            if (newDist < dist)
+            {
+                dist = newDist;
+                closest = go;
+            }
+        }
+        enemy = closest;
+
+        
+    }
 }
--- Assets/Scripts/Character/PlayerMovement3D.cs
+++ Assets/Scripts/Character/PlayerMovement3D.cs
@@ -6,6 +6,8 @@ public class PlayerMovement3D : MonoBehaviour
 {
     [SerializeField] GameObject camera;
 
+    [SerializeField] FollowPlayer cameraFollowPlayer;
+
     [SerializeField] float speed;
 
     private Rigidbody rb;
@@ -38,37 +40,6 @@ public class PlayerMovement3D : MonoBehaviour
     {
 
         MoveInDirectionOfInput();
-
-            /*
-        horizontalValue = Input.GetAxis("Horizontal");
-        verticalValue = Input.GetAxis("Vertical");
-        //camera forward and right vectors:
-        var forward = camera.transform.forward;
-        var right = camera.transform.right;
-
-        //project forward and right vectors on the horizontal plane (y = 0)
-        forward.y = 0f;
-        right.y = 0f;
-        forward.Normalize();
-        right.Normalize();
-
-        //this is the direction in the world space we want to move:
-        var desiredMoveDirection = forward * verticalValue + right * horizontalValue;
-
-        //now we can apply the movement:
-        transform.Translate(desiredMoveDirection * speed * Time.deltaTime);
-
-        /*
-        Quaternion targetRotation = Quaternion.LookRotation(transform.forward);
-        targetRotation = Quaternion.RotateTowards(
-            transform.rotation,
-            targetRotation,
-            360 * 3 * Time.deltaTime
-            );
-        
-
-        if (focus) return;
-        rb.MoveRotation(targetRotation);*/
         
     }
     public void MoveInDirectionOfInput()
@@ -78,19 +49,25 @@ public class PlayerMovement3D : MonoBehaviour
         dir.x = Input.GetAxis("Horizontal");
         dir.z = Input.GetAxis("Vertical");
 
-        Vector3 camDirection = Camera.main.transform.rotation * dir; //This takes all 3 axes (good for something flying in 3d space)    
-        targetDirection = new Vector3(camDirection.x, 0, camDirection.z); //This line removes the "space ship" 3D flying effect. We take the cam direction but remove the y axis value
+        Vector3 camDirection = Camera.main.transform.rotation * dir;
+        targetDirection = new Vector3(camDirection.x, 0, camDirection.z);
 
-        if (dir != Vector3.zero)
-        { //turn the character to face the direction of travel when there is input
+        if (dir != Vector3.zero && !cameraFollowPlayer.focussingCam)
+        {
             transform.rotation = Quaternion.Slerp(
             transform.rotation,
             Quaternion.LookRotation(targetDirection),
             Time.deltaTime * 14
             );
+        } else if (cameraFollowPlayer.focussingHeld)
+        {
+            Vector3 targetPostition = new Vector3(cameraFollowPlayer.enemy.transform.position.x,
+                                        transform.position.y,
+                                        cameraFollowPlayer.enemy.transform.position.z);
+            transform.LookAt(targetPostition);
         }
 
-        rb.velocity = targetDirection.normalized * speed;     //normalized prevents char moving faster than it should with diagonal input
+        rb.velocity = targetDirection.normalized * speed;
 
     }
 }
